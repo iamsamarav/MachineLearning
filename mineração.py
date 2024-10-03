89a823ee0111d2958ddecc9aea7c4b8f78b39b06
Samara Vitória <samiiivits@gmail.com>
2024-10-03 09:01:29 -0300

    Modificando a estrutura

--- mineração.py
+++ mineração.py
@@ -1,6 +1,40 @@
 import nltk 
-from base import base
-nltk.download('popular')
+nltk.download('rslp')
+
+base = [('estou muito feliz', 'emoção positiva'),
+        ('sou uma pessoa muito feliz', 'emoção positiva'),
+        ('tudo bem comigo', 'emoção positiva'),
+        ('alegria é o meu lema', 'emoção positiva'),
+        ('comigo está tudo ok', 'emoção positiva'),
+        ('muito bom ser amado', 'emoção positiva'),
+        ('estou empolgado em começar', 'emoção positiva'),
+        ('fui elogiado por meu trabalho', 'emoção positiva'),
+        ('vencemos a partida', 'emoção positiva'),
+        ('ganhei um presente', 'emoção positiva'),
+        ('recebi uma promoção de cargo', 'emoção positiva'),
+        ('estou bem, obrigada', 'emoção positiva'),
+        ('o dia está muito bonito', 'emoção positiva'),
+        ('estou bem, obrigado', 'emoção positiva'),
+        ('me sinto satisfeito', 'emoção positiva'),
+        ('fui aprovado', 'emoção positiva'),
+        ('de bem com a vida', 'emoção positiva'),
+        ('fui bem recebido em casa', 'emoção positiva'),
+        ('estou com medo', 'emoção negativa'),
+        ('estou com muito medo', 'emoção negativa'),
+        ('estou um pouco triste', 'emoção negativa'),
+        ('isto me deixou com raiva', 'emoção negativa'),
+        ('fui demitida', 'emoção negativa'),
+        ('esta comida está horrível', 'emoção negativa'),
+        ('tenho pavor disso', 'emoção negativa'),
+        ('de mal a pior', 'emoção negativa'),
+        ('estou incomodado', 'emoção negativa'),
+        ('estou sentindo dor', 'emoção negativa'),
+        ('perdi a aposta', 'emoção negativa'),
+        ('fui enganada', 'emoção negativa'),
+        ('fiquei desmotivada com o resultado', 'emoção negativa'),
+        ('que situação agoniante', 'emoção negativa'),
+        ('estou doente', 'emoção negativa'),
+        ('fui reprovado', 'emoção negativa')]
 
 stopwords = nltk.corpus.stopwords.words('portuguese') #Palavras irrelevantes
 
@@ -18,5 +52,7 @@ def reduzpalavras(texto): #Faz-se a remoção do sufixo extraindo o radical da p
         reduzidas = [str(steemer.stem(p)) for p in palavras.split() if p not in stopwords]
         frases_redux.append((reduzidas, emocao))
     return frases_redux
+
+frases_reduzidas = reduzpalavras(base)
 
 
