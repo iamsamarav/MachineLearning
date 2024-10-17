d90e4e5d2e22bfb95209f4394aa3f4b150e513e4
Samara Vitória <samiiivits@gmail.com>
2024-10-17 10:16:00 -0300

    feat: plotando com matplotlib

--- Iris Dataset.py
+++ Iris Dataset.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
 base = load_iris()
 print(base.data) #Retorno das medições
@@ -35,6 +36,23 @@ previsor = knn.predict(eteste)
 #Metricas avaliativos de um rede neural
 
 margem_acertos = metrics.accuracy_score(steste, previsor)
+
+#Teste com o for alterando o valor de n_neighbors
+
+valores_k = {}
+k = 1
+while k < 25:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(etreino, streino)
+    previsores_k = knn.predict(eteste)
+    acertos = metrics.accuracy_score(steste, previsores_k)
+    valores_k[k] = acertos
+    k +=1
+    
+plt.plot(list(valores_k.keys()),list(valores_k.values()))
+
+
+
 
 
 
